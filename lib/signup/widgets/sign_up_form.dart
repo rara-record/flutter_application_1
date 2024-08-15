2d3333c65037b4d177cc87241df4b1562e619d03
BORA <a52447879@gmail.com>
2024-08-15 16:16:15 +0900

    로그인 진행 애니메이션

--- lib/signup/widgets/sign_up_form.dart
+++ lib/signup/widgets/sign_up_form.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'animated_progress_indicator.dart';
+
 class SignUpForm extends StatefulWidget {
   const SignUpForm({super.key});
 
@@ -16,12 +18,35 @@ class _SignupFormState extends State<SignUpForm> {
     Navigator.of(context).pushNamed('/home');
   }
 
+  double _formProgress = 0;
+
+  void _updateFormProgress() {
+    var progress = 0.0;
+    final controllers = [
+      _firstNameTextController,
+      _lastNameTextController,
+      _usernameTextController
+    ];
+
+    for (final controller in controllers) {
+      if (controller.value.text.isNotEmpty) {
+        progress = progress + (1 / controllers.length);
+      }
+    }
+
+    setState(() {
+      _formProgress = progress;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Form(
+      onChanged: _updateFormProgress,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          AnimatedProgressIndicator(value: _formProgress),
           Text('Sign up', style: Theme.of(context).textTheme.headlineMedium),
           Padding(
             padding: const EdgeInsets.all(8),
@@ -57,7 +82,7 @@ class _SignupFormState extends State<SignUpForm> {
                     : Colors.blue;
               }),
             ),
-            onPressed: _showWelcomeScreen,
+            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
             child: const Text('Sign up'),
           ),
         ],
